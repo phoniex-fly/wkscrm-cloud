e085bc18c70e27b4f5edad71dd5bdf717c2a3e68
shixiaoyan <shixiaoyan@sinosoft.com.cn>
2021-08-27 09:34:52 +0800

    1、监控中心启动失败问题修复 2、实施部署说明文档增加性能测试及参数优化相关说明

--- wkscrm-cloud/wkscrm-server/server-monitor/src/main/java/com/wkscrm/modules/monitor/ServerMonitorApplication.java
+++ wkscrm-cloud/wkscrm-server/server-monitor/src/main/java/com/wkscrm/modules/monitor/ServerMonitorApplication.java
@@ -1,9 +1,11 @@
 package com.wkscrm.modules.monitor;
 
+import com.wkscrm.common.security.annotation.EnableWkscrmFeignClients;
 import com.yomahub.tlog.core.enhance.AspectLogEnhance;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 监控中心
@@ -11,7 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author
  */
 @EnableAdminServer
-@SpringBootApplication
+@EnableWkscrmFeignClients
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ServerMonitorApplication {
     static {AspectLogEnhance.enhance();}//进行日志增强，自动判断日志框架
     public static void main(String[] args) {
